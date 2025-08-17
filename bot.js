349b3f2b79e767a4244ba461d92af21aa47d6b41
Bùi Nguyễn Minh Nhân <minhnhanbuinguyen@gmail.com>
2025-08-17 18:30:15 +0700

    add /geo command

--- bot.js
+++ bot.js
@@ -132,7 +132,6 @@ const commands = [
                     option.setName("lon").setDescription("Nhập kinh độ").setRequired(true)
                 )
         )
-        .toJSON(),
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
