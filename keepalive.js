6238caf49bd98e444619e15fca6f4ec265c0f503
Bùi Nguyễn Minh Nhân <minhnhanbuinguyen@gmail.com>
2025-08-17 16:22:34 +0700

    refresh

---
+++ keepalive.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log("Bot is alive.");
+  res.send("<h1>Bot is alive.</h1><p>And there is no error in it :)</p>");
+});
+app.listen(process.env.PORT != undefined ? process.env.PORT : 3000);
