98f81264ed54f343b6b925f2c5bfa72f5cf190bb
Bùi Nguyễn Minh Nhân <minhnhanbuinguyen@gmail.com>
2025-08-19 20:33:47 +0700

    add 24/24 command for render

--- keepalive.js
+++ keepalive.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
   console.log("Bot is alive.");
   res.send("<h1>Bot is alive.</h1><p>And there is no error in it :)</p>");
 });
-app.listen(process.env.PORT != undefined ? process.env.PORT : 3000);
+
+app.get('/healthz', (req, res) => res.sendStatus(200));
+app.listen(process.env.PORT != undefined ? process.env.PORT : 3000, () => {
+  console.log(`Health check tại http://localhost:${process.env.PORT || 3000}/healthz`);
+});
